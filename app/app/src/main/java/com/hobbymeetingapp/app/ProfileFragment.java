cf11fba14b0dffde0f31dd1dcf037e3c9fa265bf
piotrek281097 <48327415+piotrek281097@users.noreply.github.com>
2019-10-15 21:32:38 +0200

    MyProfile zrobiony (#6)
    * MyProfile zrobiony
    
    * dodanie oceny z gwiazdek

--- app/app/src/main/java/com/hobbymeetingapp/app/ProfileFragment.java
+++ app/app/src/main/java/com/hobbymeetingapp/app/ProfileFragment.java
@@ -4,15 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ListView listOfInterests = view.findViewById(R.id.listOfInterests);
+
+        ArrayList<String> interests = new ArrayList<String>();
+        interests.add("Siatkowka");
+        interests.add("Pilka nozna");
+        interests.add("Szachy");
+        interests.add("Warcaby");
+        interests.add("Pilka nozna");
+        interests.add("Szachy");
+        interests.add("Warcaby");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(Objects.requireNonNull(getContext()), android.R.layout.simple_list_item_1, interests);
+        listOfInterests.setAdapter(adapter);
+
+        return view;
     }
+
 }
